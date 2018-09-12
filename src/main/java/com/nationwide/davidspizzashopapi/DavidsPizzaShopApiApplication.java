45e9c799aed97b949b1beb608309ca1a5f2c97f6
Isovitsch, David <ISOVITD@nationwide.com>
2018-09-12 12:23:28 -0400

    Initial version

---
+++ src/main/java/com/nationwide/davidspizzashopapi/DavidsPizzaShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.nationwide.davidspizzashopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidsPizzaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidsPizzaShopApiApplication.class, args);
+	}
+}
