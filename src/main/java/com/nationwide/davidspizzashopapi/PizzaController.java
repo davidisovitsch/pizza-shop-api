5578d3aff48c42bd36ca9382914ebdb0d5175132
Isovitsch, David <ISOVITD@nationwide.com>
2018-09-12 13:48:00 -0400

    Add controller

---
+++ src/main/java/com/nationwide/davidspizzashopapi/PizzaController.java
@@ -0,0 +1,14 @@
+package com.nationwide.davidspizzashopapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PizzaController {
+
+	@RequestMapping("/pizzas")
+	public String getPizzas() {
+		return "Pizza is good!";
+	}
+
+}
